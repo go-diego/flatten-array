353ae8daf98bca6de6fb25608487bc76c048bc7b
Diego Bernal <diego91bernal@gmail.com>
2018-11-05 15:11:40 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function flatten(inputArray) {
         if (Array.isArray(item)) {
             /**
              * If item is an array,
-             * use spread operator to recursively flatten and merge array into flattenedArray
+             * recursively flatten and merge array into flattenedArray using spread operator
              */
 
             flattenedArray = [...flattenedArray, ...[].concat(...flatten(item))];
